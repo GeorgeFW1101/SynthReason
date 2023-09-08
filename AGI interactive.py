9da68bb6cf0e8bf4e9eb74c98486678c8f7e3e63
GeorgeFW1101 <138554410+GeorgeFW1101@users.noreply.github.com>
2023-09-08 13:32:00 +1000

    v1.6 XX

--- AGI interactive.py
+++ AGI interactive.py
@@ -1,4 +1,4 @@
-# SynthReason - Synthetic Dawn - AGI - intelligent symbolic manipulation system - 1.5
+# SynthReason - Synthetic Dawn - AGI - intelligent symbolic manipulation system - 1.6
 # BSD 2-Clause License
 # 
 # Copyright (c) 2023, George Wagenknecht
@@ -26,42 +26,75 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
 import random
 import re
-token = " to "
-size = 100
-with open("fileList.conf", encoding='ISO-8859-1') as f:
-    files = f.read().splitlines()
-print("SynthReason - Synthetic Dawn")
-with open("questions.conf", encoding='ISO-8859-1') as f:
-    questions = f.read().splitlines()
-filename = "Compendium#" + str(random.randint(0, 10000000)) + ".txt"
-random.shuffle(questions)
-while(True):
-    user = re.sub('\W+', ' ', input("USER: ").lower())
-    random.shuffle(files)
-    for file in files:
-        with open(file, encoding='UTF-8') as f:
-            text = f.read()
-        output = []
-        words = user.split()
-        sentences = text.split(token)
-        random.shuffle(sentences)
-        for i in range(size):
-            output.append(' '.join(sentences[i].split()[0:4]) + " ")
-        for i in range(0, len(sentences) - 3, 3):
-            for word in words:
-                for item in set.union(set(sentences[i-1].split(word))
-                ,set(sentences[i].split(word))
-                 ,set(sentences[i+1].split(word))):             
-                    index_before = ' '.join(output).rfind(item)
-                    index_after = sentences[i+1].find(item)
-                    dot = sentences[i].find(".")
-                    comma = sentences[i].find(",")
-                    if index_before > comma and index_after < dot:
-                        if index_before == index_after+1:
-                            output.append(' '.join(sentences[i+1].split(word)[0].split()[index_before:index_before+4]) + " ")
-                            break        
-        output = ''.join(output)        
-        print("\nusing:", file.strip(), "answering:", user, "\nAI:", output, "\n\n")
-        with open(filename, "a", encoding="utf8") as f:
-            f.write("\nusing: " + file.strip() + " answering: " + user + "\n" + output + "\n")
-        break
+
+def read_file_lines(filename, encoding='ISO-8859-1'):
+    with open(filename, encoding=encoding) as file:
+        return file.read().splitlines()
+
+def generate_output_filename():
+    return "Compendium#" + str(random.randint(0, 10000000)) + ".txt"
+
+def shuffle_and_get_random_item(items):
+    random.shuffle(items)
+    return items[0]
+
+def process_user_input():
+    user_input = input("USER: ").lower()
+    return re.sub('\W+', ' ', user_input)
+
+def main():
+    token = " to "
+    size = 100
+    files = read_file_lines("fileList.conf", encoding='ISO-8859-1')
+    print("SynthReason - Synthetic Dawn")
+    questions = read_file_lines("questions.conf", encoding='ISO-8859-1')
+    filename = generate_output_filename()
+    
+    while True:
+        user = process_user_input()
+        random.shuffle(files)
+        
+        for file in files:
+            with open(file, encoding='UTF-8') as f:
+                text = f.read()
+            
+            output = generate_output(generate_output(generate_output(user, text, token, size)[:50], text, token, size)[:50], text,token , size)
+            
+            print("\nusing:", file.strip(), "answering:", user, "\nAI:", output, "\n\n")
+            
+            with open(filename, "a", encoding="utf8") as f:
+                f.write("\nusing: " + file.strip() + " answering: " + user + "\n" + output + "\n")
+                
+            break  # Remove this line if you want to process all files
+
+def generate_output(user, text, token, size):
+    output = []
+    words = user.split()
+    sentences = text.split(token)
+    shuffle_and_get_random_item(sentences)
+    
+    for i in range(size):
+        output.append(' '.join(sentences[i].split()[0:4]) + " ")
+    
+    for i in range(0, len(sentences) - 3, 3):
+        for item in words:
+            intersection = set.difference(set(set.intersection(set(sentences[i - 1].split(item)),
+                                             set(sentences[i].split(item)),
+                                             set(' '.join(output).split()))),
+                                             set(sentences[i].split(item)),
+                                             set(' '.join(output).split()))
+            for word in intersection:
+                index_before = ' '.join(output).rfind(item)
+                index_after = sentences[i + 1].find(item)
+                dot = ' '.join(output).index(word)
+                comma = sentences[i].find(",")
+                
+                if index_before > comma and index_after < dot:
+                    if index_before == index_after + 1:
+                        output.append(' '.join(sentences[i + 1].split(item)[0].split()[dot:dot + 4]) + " ")
+    
+    return ''.join(output)
+
+if __name__ == "__main__":
+    main()
+
