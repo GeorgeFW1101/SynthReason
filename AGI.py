c3999a879f038fa3c512dad970e07c1004955f80
GeorgeFW1101 <138554410+GeorgeFW1101@users.noreply.github.com>
2023-09-09 19:30:20 +1000

    v1.7 XX

--- AGI.py
+++ AGI.py
@@ -24,6 +24,32 @@
 # CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
+# SynthReason - Synthetic Dawn - AGI - intelligent symbolic manipulation system - 1.7
+# BSD 2-Clause License
+# 
+# Copyright (c) 2023, George Wagenknecht
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
 import random
 import re
 import math
@@ -36,8 +62,8 @@ with open("questions.conf", encoding='ISO-8859-1') as f:
     questions = f.read().splitlines()
 filename = "Compendium#" + str(random.randint(0, 10000000)) + ".txt"
 random.shuffle(questions)
-while(True):
-    user = re.sub('\W+', ' ', input("USER: ").lower())
+for question in questions:
+    user = re.sub('\W+', ' ', question.lower())
     random.shuffle(files)
     for file in files:
         with open(file, encoding='UTF-8') as f:
@@ -45,7 +71,7 @@ while(True):
         output = []
         words = user.split()
         sentences = text.split(token)
-        random.shuffle(sentences)
+        random.shuffle(sentences)        
         for i in range(size):
             output.append(' '.join(sentences[i].split()[0:4]) + " ")
         for i in range(0, len(sentences) - 3, 3):
