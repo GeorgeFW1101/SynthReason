4bff145c0146fbd2a3e79cde5941f281a86330cd
GeorgeFW1101 <138554410+GeorgeFW1101@users.noreply.github.com>
2023-09-18 19:37:17 +1000

    v3.0 XX

--- AGI interactive.py
+++ AGI interactive.py
@@ -1,4 +1,4 @@
-# SynthReason - Synthetic Dawn - AGI - intelligent symbolic manipulation system - 2.8
+# SynthReason - Synthetic Dawn - AGI - intelligent symbolic manipulation system - 3.0
 # BSD 2-Clause License
 # 
 # Copyright (c) 2023, George Wagenknecht
@@ -27,32 +27,54 @@
 import random
 import re
 import math
+
 token = " and "
 size = 25
+
 with open("fileList.conf", encoding='ISO-8859-1') as f:
-        files = f.read().splitlines()
+    files = f.read().splitlines()
+
 print("SynthReason - Synthetic Dawn")
+
 with open("questions.conf", encoding='ISO-8859-1') as f:
     questions = f.read().splitlines()
+
 while True:
     user = re.sub('\W+', ' ', input("USER: ").lower())
-    random.shuffle(files)    
+    random.shuffle(files)
+
     for file in files:
         with open(file, encoding='UTF-8') as f:
             text = f.read()
+
         sentences = text.split(token)
         sentences = [sentence for sentence in sentences if any(word in sentence for word in user.split())]
         random.shuffle(sentences)
-        sine_frequency, cosine_frequency, amplitude, phase = 1.2, 0.8, 0.5, 1.1  # Adjust as needed        
-        sine_values = [amplitude / math.cos(4 / math.pi / sine_frequency * i + phase) for i in reversed(range(len(sentences)))]
-        degrees_values = [amplitude / math.degrees(2 /math.pi / cosine_frequency * i + phase) for i in reversed(range(len(sentences)))]        
-        combined_wave = [s +c for s, c in zip(sine_values, degrees_values)]        
-        sentences_with_wave = sorted(zip(sentences, combined_wave), key=lambda x: x[1], reverse=True)        
+
+        sine_frequency, cosine_frequency, amplitude, phase = 5.2, 0.5, 0.4, 1.1  # Adjust as needed
+
+        sine_values = [amplitude / math.atan(4 / math.pi / sine_frequency * i + phase) for i in reversed(range(len(sentences)))]
+        degrees_values = [amplitude / math.degrees(2 / math.pi / cosine_frequency * i + phase) for i in sine_values]
+
+        combined_wave = [s + c for s, c in zip(sine_values, degrees_values)]
+
+        # Define a custom sorting function based on the wave values
+        def custom_sort(item):
+            sentence, wave_value = item
+    # First, sort by wave_value in descending order
+    # If wave values are equal, then sort by sentence length in ascending order
+            return (-wave_value, len(sentence))
+
+
+        sentences_with_wave = sorted(zip(sentences, combined_wave), key=custom_sort, reverse=True)
         selected_sentences = [' '.join(sentence.split()[:3]) for sentence, _ in sentences_with_wave][:size]
-        output_with_wave = ' '.join(selected_sentences)      
-        print("\nusing:", file.strip(), "answering:", user, "\nAI:", output_with_wave, "\n\n")        
+
+        output_with_wave = ' '.join(selected_sentences)
+
+        print("\nusing:", file.strip(), "answering:", user, "\nAI:", output_with_wave, "\n\n")
+
         filename = "Compendium#" + str(random.randint(0, 10000000)) + ".txt"
         with open(filename, "a", encoding="utf8") as f:
             f.write("\nusing: " + file.strip() + " answering: " + user + "\n" + output_with_wave + "\n")
-        
+
         break
